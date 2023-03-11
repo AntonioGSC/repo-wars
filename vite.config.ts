1330a0fefd6c930126caf38eec8dc2266627c8dc
AntonioGSC <antoniogabrielagsc@gmail.com>
2023-03-11 18:36:10 -0300

    feat: fix: update responsive layout of landing page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {alias: { 'node-fetch': 'axios' }},
-  define: {
-    global: {}
-  },
+  define: {},
 })
