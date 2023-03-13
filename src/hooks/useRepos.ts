8157498db66b23a2b8a9dc20b8b8f1ebedee6715
AntonioGSC <antoniogabrielagsc@gmail.com>
2023-03-12 22:02:10 -0300

    feat: add types for Repos

--- src/hooks/useRepos.ts
+++ src/hooks/useRepos.ts
@@ -1,6 +1,12 @@
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { githubClient } from '../api';
 
+type UseReposReturnType = [
+    repositories: Repo[],
+    setRepositories: Dispatch<SetStateAction<Repo[]>>,
+    allRepos: Repo[],
+]
+
 const randomSince = Math.floor(Math.random() * 1000000);
 
 const fetchRepos = async (amount: number, setters: Dispatch<SetStateAction<any[]>>[]) => {
@@ -15,11 +21,11 @@ const fetchRepos = async (amount: number, setters: Dispatch<SetStateAction<any[]
     setters.forEach(setter => setter(res.data.items))
 }
 
-const useRepos = (amount: number) => {
+const useRepos = (amount: number) : UseReposReturnType => {
     // cria um estado para todos os repositorios
-    const [allRepositories, setAllRepositories] = useState<any>([])
+    const [allRepositories, setAllRepositories] = useState<Repo[]>([])
     // cria um estado para os repositorios
-    const [repositories, setRepositories] = useState<any>([])
+    const [repositories, setRepositories] = useState<Repo[]>([])
     // useEffect para recuperar os repositorios 
     useEffect(() => {
         fetchRepos(amount, [setAllRepositories, setRepositories])
