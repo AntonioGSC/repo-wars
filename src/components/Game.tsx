f2a8e9cb252fcc684e2c08be8dff84ccc2c6858e
AntonioGSC <antoniogabrielagsc@gmail.com>
2023-03-14 13:38:15 -0300

    feat: add game logic to GameComponent

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -14,17 +14,49 @@ const Game: React.FC<GameProps> = ({
     originalList,
     setShowGame,
 }) => {
+    const [repos, setRepos] = repoState;
     const [over, setOver] = useState<boolean>(false);
     const [repo1, setRepo1] = useState<Repo | null>(null);
     const [repo2, setRepo2] = useState<Repo | null>(null);
 
+    const randomSelect = (arr: Repo[]) => {
+        if (arr.length < 2) {
+            setRepos(originalList);
+            setOver(true);
+            return;
+        }
+        const generateRandom = () => Math.floor(Math.random() * arr.length);
+        const randomIndex1 = generateRandom()
+        let randomIndex2 = generateRandom()
+
+        while (randomIndex1 ===  randomIndex2) {
+            randomIndex2 = generateRandom()
+        }
+
+        const randomRepo1 = arr[randomIndex1];
+        const randomRepo2 = arr[randomIndex2];
+        
+        const updatedArray = arr.filter(repo => repo.id !== randomRepo1.id && repo.id !== randomRepo2.id);
+        
+        setRepo1(randomRepo1);
+        setRepo2(randomRepo2);
+        setRepos(updatedArray);
+    }
+
     useEffect(() => {
-        setRepo1(originalList[0])
-        setRepo2(originalList[1])
+        randomSelect(repos)
     }, [])
 
-    const handleChoice = () => {
-        
+    const handleChoice = (r: Repo) => {
+        const choosenRepo = r.id === repo1?.id ? repo1 : repo2;
+        const winner = repo1?.stargazers_count! > repo2?.stargazers_count! ? repo1 : repo2;
+
+        if (choosenRepo?.id === winner?.id) {
+            alert('Correct!');
+        } else {
+            alert('Incorrect!');
+        }
+        randomSelect(repos);
     }
 
     return <div className='game'>
@@ -32,7 +64,7 @@ const Game: React.FC<GameProps> = ({
             <div className='score'></div>
         ) : (
             repo1 && repo2 && <div className='repos'>
-                <h1 className='title'>Choose the Repo with most start</h1>
+                <h1 className='title'>Choose the Repo with most stars</h1>
                 <div className='container'>
                     <RepoCard content={repo1} handler={handleChoice} />
                     <div className='dashboard'>
@@ -46,7 +78,7 @@ const Game: React.FC<GameProps> = ({
             <button type='submit' className='button gradient alternate' onClick={() => setShowGame(false)}>{over ? 'Play Again' : 'Back'}</button>
             {
                 !over && (
-                    <button type='submit' className='button gradient' onClick={() => setShowGame(false)}>Skip</button>
+                    <button type='submit' className='button gradient' onClick={() => randomSelect(repos)}>Skip</button>
                 )
             }
         </div>
